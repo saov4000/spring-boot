244720ad3562f1132e8e80b43760983d90f5410e
saov4000 <saov4000@gmail.com>
2024-07-03 10:49:19 -0300

    second

---
+++ src/main/java/com/saov/aula/entities/User.java
@@ -0,0 +1,88 @@
+package com.saov.aula.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.fasterxml.jackson.core.SerializableString;
+
+public class User implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	private long id;
+	private String name;
+	private String email;
+	private String prone;
+	private String password;
+	
+	public User() {
+		
+	}
+	
+	public User(long id, String name, String email, String prone, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.prone = prone;
+		this.password = password;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getProne() {
+		return prone;
+	}
+
+	public void setProne(String prone) {
+		this.prone = prone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return id == other.id;
+	}
+	
+	
+}
---
+++ src/main/java/com/saov/aula/resources/UserResources.java
@@ -0,0 +1,20 @@
+package com.saov.aula.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.saov.aula.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L,"maria","m@m","9999","123");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
