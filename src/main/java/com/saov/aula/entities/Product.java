6f5cbab08495adf793daaa3f33d62816aefd65aa
saov4000 <saov4000@gmail.com>
2024-07-07 12:53:59 -0300

    9th

--- src/main/java/com/saov/aula/entities/Product.java
+++ src/main/java/com/saov/aula/entities/Product.java
@@ -9,6 +9,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
@@ -18,14 +21,17 @@ public class Product implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String description;
 	private Double price;
 	private String imgUrl;
 	
-	@Transient
+	@ManyToMany
+	@JoinTable(name = "tb_product_category",
+	joinColumns = @JoinColumn(name="product_id"),
+	inverseJoinColumns = @JoinColumn(name="category_id"))
 	private Set<Category> categories = new HashSet<>();
 	
 	public Product() {
--- src/main/java/com/saov/aula/resources/ProductResource.java
+++ src/main/java/com/saov/aula/resources/ProductResource.java
@@ -30,7 +30,4 @@ public class ProductResource {
 		Product obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
-	
-	
 }
